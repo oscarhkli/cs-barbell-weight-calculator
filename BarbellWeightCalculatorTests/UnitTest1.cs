4edba316f1a1f0a6fb1fbbf22d16c8a8fe236b12
Oscar Li <learning@oscarhkli.com>
2025-04-28 21:40:13 +0100

    feat: init BarbellWeightCalculator

---
+++ BarbellWeightCalculatorTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace BarbellWeightCalculatorTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
